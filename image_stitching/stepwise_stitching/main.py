9739a88feab908778093bfad4a577cd47f9f466d
vijendra1125 <vijendrasingh003@gmail.com>
2020-08-25 19:52:41 +0530

    just backing up

--- image_stitching/stepwise_stitching/main.py
+++ image_stitching/stepwise_stitching/main.py
@@ -17,18 +17,12 @@ import core
 def main():
     # read images
     images = utils.read_images()
-    images_new = [images[0], images[1]]
+    images_new = [images[0][1], images[1][1]]
     # feature finding and image matching
     h_mat = core.find_homography(images_new)
     # stitch image
     if h_mat is not None:
         core.stitch(images_new, h_mat[0][2], h_mat[0][3])
-    # stitched = cv2.warpPerspective(
-    #     images_new[0], h_mat[0][2], tuple(2*x for x in images_new[0].shape))
-    # cv2.imshow('stitched', stitched)
-    # cv2.moveWindow('stitched', 100, 50)
-    # cv2.waitKey(0)
-    # cv2.destroyWindow('stitched')
 
 
 if __name__ == '__main__':
--- image_stitching/stepwise_stitching/utilities.py
+++ image_stitching/stepwise_stitching/utilities.py
@@ -11,20 +11,22 @@ import parameters as params
 
 def read_images(dir_name='data'):
     '''
-    @brief: 
-    @args[in]:
-    @args[out]:
+    @brief: read images and store it along with unique ID representing its position
+    @args[in]: directory containing images
+    @args[out]: list of images and its corresping assigned ID
     '''
     images = []
     image_names = os.listdir(dir_name)
     image_names.sort()
-    for image_name in image_names:
+    for cam_num, image_name in enumerate(image_names):
         print(image_name, 'loaded')
         image = cv2.imread(os.path.join(dir_name, image_name), 0)
         width = image.shape[1]//params.DOWN_FACTOR
         height = image.shape[0]//params.DOWN_FACTOR
         image = cv2.resize(image, (width, height))
-        images.append(image)
+        row = cam_num//params.VIEW_ROWS
+        col = cam_num % params.VIEW_COLS
+        images.append([(row, col), image])
         if params.TEST_BOOL:
             cv2.imshow(image_name, image)
             cv2.moveWindow(image_name, 100, 50)
